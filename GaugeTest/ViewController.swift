1a8a5d367e771f1cf1eef29b31e91832c0117c4e
Kimchheang <Kimchheang.heng@okoone.com>
2018-10-16 12:14:20 +0700

    Initial Commit

---
+++ GaugeTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GaugeTest
+//
+//  Created by Kimchheang on 10/16/18.
+//  Copyright © 2018 Kimchheang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
